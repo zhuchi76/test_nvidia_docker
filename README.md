b892cb01c65aa7177182e02e594ebd82e2be468d
tzuchichen <tzuchichen.sc08@nycu.edu.tw>
2024-09-06 15:50:16 +0800

    update install script

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
 # test_nvidia_docker github repo
 
-## Install nvidia-driver
+## Install nvidia-driver & CUDA & cuDNN
 https://medium.com/@scofield44165/ubuntu-20-04%E4%B8%AD%E5%AE%89%E8%A3%9Dnvidia-driver-cuda-11-4-2%E7%89%88-cudnn-install-nvidia-driver-460-cuda-11-4-2-cudnn-6569ab816cc5
-
-## Install cuda cudnn (official)
-https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html
-
-## Install nvidia-docker2
-Run every command in install_nvidia_docker.sh
 
 ## Download the repo
 ```bash
 git clone git@github.com:zhuchi76/test_nvidia_docker.git
 ```
 
-## Build the local docker image
+## Install docker
 ```bash
-cd Docker
+source install_docker.sh
 ```
 
+## Install nvidia-docker2
 ```bash
-source build.sh
+source install_nvidia_docker.sh
 ```
 
 ## RUN the docker image
--- install_nvidia_docker.sh
+++ install_nvidia_docker.sh
@@ -1,31 +1,10 @@
-# Docker
-sudo apt-get update
-# sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
-
-# Put the user in the docker group
-sudo usermod -a -G docker $USER
-newgrp docker
-
 # Nvidia Docker
 sudo apt install curl
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-distribution="ubuntu20.04"
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# distribution="ubuntu20.04"
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
 
 sudo apt-get update 
 sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
-
-## Erase all Docker images [!!! CAUTION !!!]
-# docker rmi -f $(docker images -a -q)
-
-## Erase one Docker image  [!!! CAUTION !!!]
-# docker ps
-# docker rmi -f image_id
-
